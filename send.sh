f196ffc03a2394f8adb00c599074d58c93a8d11d
baiki <dot_baiki@yahoo.com>
2013-06-16 19:59:49 +0200

    cosmetics only

--- send.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-clear
-scp * baiki@bello.local:/home/baiki/Desktop/project_thermometer/.
-
-#scp messwerte* baiki@bello.local:/home/baiki/Desktop/project_thermometer/.
-#scp thermometer.py baiki@bello.local:/home/baiki/Desktop/project_thermometer/thermometer.py
-#scp send.sh baiki@bello.local:/home/baiki/Desktop/project_thermometer/.
