6db777e9cc69e42927ddcc6debee58acaff1e86e
baiki <dot_baiki@yahoo.com>
2013-06-24 12:50:31 +0200

    added WTFPL in code

--- thermometer.py
+++ thermometer.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-Copyright © 2013 Your Name <dot_baiki@yahoo.com>
+Copyright © 2013 Baiki <dot_baiki@yahoo.com>
 This work is free. You can redistribute it and/or modify it under the
 terms of the Do What The Fuck You Want To Public License, Version 2,
 as published by Sam Hocevar. See the COPYING file for more details.
